9d407aed3c61922badfc418bcf5f6c3492f1e29b
Sneha Simkhada <simkhadasneha@gmail.com>
2022-03-22 10:00:22 -0500

    File destination

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const fs = require('fs');
 
-var user = []
+var user = [  
+  {
+  "id": 1,
+  "name": "Sneha",
+  "age": "20",
+  "gender": "Female",
+  "address": "5560 Rock Rd",
+  "phone": "109-650-8849"
+},
+{
+  "id": 2,
+  "name": "Sam",
+  "age": "28",
+  "gender": "Male",
+  "address": "5334 Hill Rd",
+  "phone": "546-650-8849"
+},
+{
+  "id": 3,
+  "name": "Sona",
+  "age": "17",
+  "gender": "Female",
+  "address": "2234 Dog Rd",
+  "phone": "109-879-8849"
+}]
 const PORT = process.env.PORT || 5000
-const fileData = JSON.parse(fs.readFileSync('/Users/sneha/Documents/SchoolWork2022_Sp/415/EMR-415/user/emr.json'))
+const fileData = JSON.parse(fs.readFileSync('./user/emr.json'))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -34,7 +58,7 @@ express()
 
     if (new_record.name && new_record.age && new_record.gender && new_record.address && new_record.phone) {
      fileData.push(new_record)
-      fs.writeFileSync('/Users/sneha/Documents/SchoolWork2022_Sp/415/EMR-415/user/emr.json', JSON.stringify(fileData, null, 2));
+      fs.writeFileSync('./user/emr.json', JSON.stringify(fileData, null, 2));
       user.push(new_record)
       console.log(new_record)
       res.sendStatus(200)
